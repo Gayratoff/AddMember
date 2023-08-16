9a53951c553e2791fc13964eed8890e2b101b564
Xudoberdi G'ayratov <115026746+Gayratoff@users.noreply.github.com>
2023-08-16 14:56:52 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from pyrogram import Client
-
 api_id = 9899039  # API ID KIRITING
 api_hash = '5bab03171ae5e0d92e3e0ce70137943c' # API HASH KIRITING
 app = Client(
@@ -9,12 +8,10 @@ app = Client(
 )
 
 app.start()
-from_chat_id = int(input("AZO OLMOQCHI BOLGAN GURUH IDSINI KIRITING: "))# A'zo tanlamoqchi bolgan guruhingiz ID raqamini kiritng
-my_chat_id = int(input("AZO QO'SHMOQCHI BOLGAN GURUH IDSINI KIRITING: "))
 # DASTURCHI @MrGayratov Kanal : @KingsOfPy
 while True:
-    from_chat_id = from_chat_id # A'zo tanlamoqchi bolgan guruhingiz ID raqamini kiritng
-    my_chat_id = my_chat_id # A'zolarni qo'shmoqchi bo'lgan guruhingiz ID raqamini kiritng
+    from_chat_id = -1001050555471 # A'zo tanlamoqchi bolgan guruhingiz ID raqamini kiritng
+    my_chat_id = -1001825384988 # A'zolarni qo'shmoqchi bo'lgan guruhingiz ID raqamini kiritng
     members = [] # Foidalanuvchi idlarini ro'yxatga ko'chirish yani olish uchun
     # DASTURCHI @MrGayratov Kanal : @KingsOfPy
     for member in app.get_chat_members(from_chat_id):
